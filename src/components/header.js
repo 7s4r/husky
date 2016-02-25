5bebf7d11e1bc54f7edb55fca93adc07b8fb6a27
Gueorgui AGAPOV <7s4r@users.noreply.github.com>
2016-02-25 17:58:51 +0100

    added new components

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className='mdl-layout__header' role='banner'>
+        <div className='mdl-layout__drawer-button'>
+          <i className='material-icons'>menu</i>
+        </div>
+      </header>
+    )
+  }
+}
--- src/components/husky.js
+++ src/components/husky.js
@@ -1,8 +1,56 @@
+import React, {Component} from 'react'
 import './husky.scss'
-import React from 'react'
 
-export default React.createClass({
+export default class Husky extends Component {
   render() {
-    return <div>Husky</div>
+    return (
+      <div>
+        <div className='husky'>
+          <div className='mane'>
+            <div className='coat'></div>
+          </div>
+          <div className='body'>
+            <div className='head'>
+              <div className='ear'></div>
+              <div className='ear'></div>
+              <div className='face'>
+                <div className='eye'></div>
+                <div className='eye'></div>
+                <div className='nose'></div>
+                <div className='mouth'>
+                  <div className='lips'></div>
+                  <div className='tongue'></div>
+                </div>
+              </div>
+            </div>
+            <div className='torso'></div>
+          </div>
+          <div className='legs'>
+            <div className='front-legs'></div>
+            <div className='leg'></div>
+            <div className='leg'></div>
+          </div>
+          <div className='hind-leg'></div>
+        </div>
+        <div className='tail'>
+          <div className='tail'>
+            <div className='tail'>
+              <div className='tail'>
+                <div className='tail'>
+                  <div className='tail'>
+                    <div className='tail'></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
-})
+}
+
+React.render(
+  <Husky />,
+  document.getElementById('app')
+)
